7a55a68afe75c4233cea11c1597ad8755637bde1
user <user@user-PC>
2018-11-11 20:51:04 -0600

    avance de fin de semana, se prevee la integracion de tema gratuito para administrar(front)

--- tripleA/src/main/java/com/mbservices/tripleA/controllers/AseguradoraController.java
+++ tripleA/src/main/java/com/mbservices/tripleA/controllers/AseguradoraController.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.mbservices.tripleA.models.dao.IAseguradoraDao;
 import com.mbservices.tripleA.models.entity.Aseguradora;
+import com.mbservices.tripleA.services.AseguradoraService;
 
 @Controller
 @RequestMapping(value="/aseguradora")
 public class AseguradoraController {
 	
 	@Autowired
-	private IAseguradoraDao dao;
+	private AseguradoraService service;
 	
 	@RequestMapping(value="/listar",method=RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado de Aseguradoras");
-		model.addAttribute("lista",dao.findAll());
+		model.addAttribute("lista",service.findAll());
 		return "aseguradora/listar";
 	}
 	
@@ -41,17 +41,17 @@ public class AseguradoraController {
 		if(result.hasErrors()){
 			return "aseguradora/form";
 		}
-		dao.save(aseguradora);
-		return "redirect:listar";
+		service.save(aseguradora);
+		return "redirect:/aseguradora/listar";
 	}
 	
 	@RequestMapping(value="/form/{id}")
 	public String edit(@PathVariable Long id, Model model) {
 		Aseguradora aseguradora=null;
 		if(id>0){
-			aseguradora=dao.findById(id);
+			aseguradora=service.findById(id);
 		}else{
-			return "redirect:listar";
+			return "redirect:/aseguradora/listar";
 		}
 		model.addAttribute("titulo","FormularioAseguradora");
 		model.addAttribute("aseguradora", aseguradora);
@@ -61,8 +61,8 @@ public class AseguradoraController {
 	@RequestMapping(value="/eliminar/{id}")
 	public String delete(@PathVariable Long id){
 		if(id>0){
-			dao.delete(id);
+			service.delete(id);
 		}
-		return "redirect:listar";
+		return "redirect:/aseguradora/listar";
 	}
 }
---
+++ tripleA/src/main/java/com/mbservices/tripleA/models/idao/IAseguradoraDao.java
@@ -0,0 +1,9 @@
+package com.mbservices.tripleA.models.idao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mbservices.tripleA.models.entity.Aseguradora;
+
+public interface IAseguradoraDao extends CrudRepository<Aseguradora, Long>{
+	
+}
